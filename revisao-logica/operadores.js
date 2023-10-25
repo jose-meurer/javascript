35ee1dc6fb717bac641861be4b7859abc1a4ee4f
joseMeurer <josewmeurer@gmail.com>
2023-10-25 16:18:20 -0300

    operadores aritmeticos

---
+++ revisao-logica/operadores.js
@@ -0,0 +1,18 @@
+// = - * / % **
+
+let n1 = 10;
+let n2 = 3;
+
+/* OPERADORES ARITMÉTICOS */
+
+console.log(n1 + n2);   //adição
+console.log(n1 - n2);   //subtração
+console.log(n1 * n2);   //multiplicação
+console.log(n1 / n2);   //divisao
+console.log(n1 % n2);   //resto
+console.log(n1 ** n2);  //potencia
+
+let n3 = n1 / n2;
+console.log(n3.toFixed(2)); //reduz as casas decimais depois do ponto
+
+/* OPERADORES DE ATRIBUIÇÃO */
