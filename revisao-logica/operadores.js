fd334df3f4327fdd0cfd367c7f91c2869a4191b7
joseMeurer <josewmeurer@gmail.com>
2023-10-25 17:00:00 -0300

    operadores de atribuicao, incremento/decremento e comparacao

--- revisao-logica/operadores.js
+++ revisao-logica/operadores.js
@@ -5,14 +5,53 @@ let n2 = 3;
 
 /* OPERADORES ARITMÉTICOS */
 
-console.log(n1 + n2);   //adição
-console.log(n1 - n2);   //subtração
-console.log(n1 * n2);   //multiplicação
-console.log(n1 / n2);   //divisao
-console.log(n1 % n2);   //resto
-console.log(n1 ** n2);  //potencia
+console.log(n1 + n2); //adição
+console.log(n1 - n2); //subtração
+console.log(n1 * n2); //multiplicação
+console.log(n1 / n2); //divisao
+console.log(n1 % n2); //resto
+console.log(n1 ** n2); //potencia
 
 let n3 = n1 / n2;
 console.log(n3.toFixed(2)); //reduz as casas decimais depois do ponto
 
 /* OPERADORES DE ATRIBUIÇÃO */
+
+let n4 = 20;
+// n3 = n3 + 15;
+n4 += 15;
+console.log(n4);
+
+/* OPERADORES DE INCREMENTO E DECREMENTO */
+
+let i = 0;
+i++;
+console.log(i);
+console.log(++i);
+i = 0;
+i--;
+console.log(i);
+console.log(--i);
+
+/*  OPERADORES DE COMPARAÇÃO 
+
+    igualdade de valor ==
+    igualdade de valor e tipo ===
+    <, >, <=, >=
+    sinal de diferença !=
+    sinal de valores e tipos diferentes !==
+*/
+
+console.log(n1, n2, n4);
+let n5 = "10";
+console.log(n1 == n5);
+console.log(n1 === n5, typeof n1, typeof n5); //valores iguais mas tipos diferentes
+
+console.log(n2 < n5);
+console.log(n2 > n5);
+
+console.log(n1 <= n5);
+console.log(n1 >= n5 + 1);
+
+console.log(n1 != n5);
+console.log(n1 !== n5, typeof n1, typeof n5); //valores iguais mas tipos diferentes
