0134619a45e823799ea694710a790da177978842
joseMeurer <josewmeurer@gmail.com>
2023-10-31 10:00:00 -0300

    use-strich

---
+++ introducao/use-strict.js
@@ -0,0 +1,27 @@
+"use strich";
+// y = 10;
+
+function foo() {
+  "use strich";
+  let x = 20;
+}
+foo();
+// console.log(y);
+
+// function dobrar(n1, n1) {
+//     "use strich"
+//     console.log(n1, n1);
+//     return n1 * n1;
+// }
+// console.log(dobrar(5, 7));
+
+// function teste() {
+//     "use strich"
+//     console.log(this);
+//     this.a = "a";
+// }
+// teste()
+
+let msg = "Qualquer string";
+msg.count = 0;
+console.log(msg);
