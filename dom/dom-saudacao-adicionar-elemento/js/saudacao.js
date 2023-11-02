4fe964f80f38d016d848cc5b27676219d391f51a
joseMeurer <josewmeurer@gmail.com>
2023-11-02 09:48:41 -0300

    criar elementos DOM

---
+++ dom/dom-saudacao-adicionar-elemento/js/saudacao.js
@@ -0,0 +1,24 @@
+(function () {
+  function randomLogUser() {
+    let n = Math.floor(Math.random() * 2);
+
+    if (n) {
+      return "José";
+    } else {
+      return "";
+    }
+  }
+  const username = randomLogUser();
+
+  if (username) {
+    const topBarElement = document.createElement("div");
+    topBarElement.className = "top-bar";
+    topBarElement.innerHTML = `<p>Bem-vindo, <b>${username}</b></p>`;
+
+    const elementDad = document.querySelector(".hero");
+    elementDad.insertBefore(topBarElement, elementDad.firstElementChild);
+
+    console.log(topBarElement.title = "teta");
+  
+  }
+})();
--- dom/exercicio-saudacao/js/saudacao.js
+++ dom/exercicio-saudacao/js/saudacao.js
@@ -1,12 +1,18 @@
-(function() {
-    // const username = "José";
-    const username = "";
+(function () {
+  // const username = "José";
+  const username = "";
 
-    const _element = document.querySelector(".top-bar p");
-    if(username) {
-        _element.innerHTML += `<em>${username}</em>`;
-    }else {
-        _element.parentElement.style.display = "none";
-    }
-
-})()
+  const _element = document.querySelector(".top-bar p");
+  if (username) {
+    _element.innerHTML += `<em>${username}</em>`;
+  } else {
+    // _element.parentElement.style.display = "none";
+    /* const filho = pai.children; //Recupera o elemento filho
+    console.log(pai);
+    console.log(filho); */
+    
+    const _elementToRemove = _element.parentElement; //Recupera o elemento pai
+    //pai.remove(); //Nao funciona no explorer11
+    _elementToRemove.parentElement.removeChild(_elementToRemove);
+  }
+})();
