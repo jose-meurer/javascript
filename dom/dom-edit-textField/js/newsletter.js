538e0b526a2087b17c4688333413996ca573ce39
joseMeurer <josewmeurer@gmail.com>
2023-11-03 15:41:57 -0300

    javascript props

--- dom/dom-edit-textField/js/newsletter.js
+++ dom/dom-edit-textField/js/newsletter.js
@@ -1,10 +1,10 @@
 const txtEmail = document.getElementById("txtEmail");
 
 function editarEmail() {
-    txtEmail.disabled = false; //vscode nao autocompleta as property de atributos do html, apenas as propriedades do html
-    txtEmail.focus();   //propriedade = id, class, name, title... || atributos = controls, required, disabled...
+  txtEmail.disabled = false;
+  txtEmail.focus();
 }
 
 function disableEmail() {
-    txtEmail.disabled = true;
+  txtEmail.disabled = true;
 }
