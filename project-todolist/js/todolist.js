73909c01a7badabb980ed19b8aab0a80e5657260
joseMeurer <josewmeurer@gmail.com>
2023-11-14 17:02:12 -0300

    add renderTasks, addTask to obj, generateLiTask

--- project-todolist/js/todolist.js
+++ project-todolist/js/todolist.js
@@ -6,36 +6,69 @@
   const itemInput = document.getElementById("item-input");
   const todoAddForm = document.getElementById("todo-add");
   const ul = document.getElementById("todo-list");
-  const lis = ul.getElementsByTagName("li");
+  //   const lis = ul.getElementsByTagName("li");
+
+  //Array de objs de task
+  let arrTasks = [
+    {
+      name: "task 1",
+      createAt: Date.now(),
+      completed: false,
+    },
+    {
+      name: "task 2",
+      createAt: Date.now(),
+      completed: false,
+    },
+  ];
 
   //evento de submit da task
   todoAddForm.addEventListener("submit", (e) => {
     e.preventDefault();
     addTask(itemInput.value); //innerHTML nao é muito confiavel e tem desempenho pior
+    renderTasks();
 
     itemInput.value = "";
     itemInput.focus();
   });
 
-  [...lis].forEach((li) => addEventLi(li));
+  renderTasks();
 
-  //Cria uma nova task no dom
+  //Adiciona uma nova task no array
   function addTask(task) {
+    arrTasks.push({
+      name: task,
+      createAt: Date.now(),
+      completed: false,
+    });
+  }
+
+  //mostra as task no dom
+  function renderTasks() {
+    ul.innerHTML = "";
+    arrTasks.forEach((taskObj) => {
+      ul.appendChild(generateLiTask(taskObj));
+    });
+  }
+
+  //Cria uma nova li e retorna
+  function generateLiTask(obj) {
     const li = document.createElement("li");
     li.setAttribute("class", "todo-item");
     const p = document.createElement("p");
     p.setAttribute("class", "task-name");
-    p.textContent = task;
+    p.textContent = obj.name;
     li.appendChild(p);
-    ul.appendChild(li);
 
     addEventLi(li);
+
+    return li;
   }
 
   //Adiciona evento de click nas li
   function addEventLi(li) {
     li.addEventListener("click", (e) => {
-      console.log(e.srcElement.classList);
+      console.log(e.target.className);
     });
   }
 })();
