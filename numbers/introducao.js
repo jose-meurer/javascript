4cbe6781c0c9ba901a93c563ad9137b36addeb88
joseMeurer <josewmeurer@gmail.com>
2023-11-24 20:12:49 -0300

    Numbers toString, toLocaleString

--- numbers/introducao.js
+++ numbers/introducao.js
@@ -7,4 +7,21 @@ console.log((123).toPrecision(9));
 
 numero = 12.34567;
 console.log(numero.toExponential());
-console.log(numero.toExponential(4));//Recebe o numero de casas
+console.log(numero.toExponential(4)); //Recebe o numero de casas
+
+numero = 19;
+
+console.log(numero.toString(), typeof numero.toString()); //19
+console.log(numero.toString(2), typeof numero.toString()); //10011
+console.log(numero.toString(8), typeof numero.toString()); //2*8=16+3=19 = 23
+console.log(numero.toString(16), typeof numero.toString()); //13
+
+numero = 123456.789;
+
+console.log(numero.toLocaleString("en-US"));
+console.log(numero.toLocaleString("pt-BR"));
+
+console.log(numero.toLocaleString("pt-BR", { style: "currency", currency: "BRL" }));
+console.log(numero.toLocaleString("en-US", { style: "currency", currency: "USD" }));
+console.log(numero.toLocaleString("pt-PT", { style: "currency", currency: "EUR" }));
+console.log(numero.toLocaleString("ja-JP", { style: "currency", currency: "JPY" }));
