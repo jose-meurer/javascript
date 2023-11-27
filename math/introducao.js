ea934e35b82aa2c52aef5c70f7b92f21c952607f
joseMeurer <josewmeurer@gmail.com>
2023-11-27 20:33:03 -0300

    Math min, max, round, ceil, floor, pow, sqrt, cbrt, random

---
+++ math/introducao.js
@@ -0,0 +1,21 @@
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+const arr = new Array(1, 2, 4, 5, 7, 10);
+console.log(arr);
+console.log(Math.min(...arr));
+console.log(Math.max(...arr));
+
+console.log(Math.round(49.50001));
+console.log(Math.round(49.49)); //Arredonda na casa de 0.5
+
+console.log(Math.ceil(49.2)); //Arredonda sempre para cima
+console.log(Math.floor(49.2)); //Arredonda sempre para baixo
+
+console.log(Math.pow(5, 3));
+console.log(5 ** 3);
+
+console.log(Math.sqrt(49)); //Retorna raiz quadrada
+console.log(Math.cbrt(8));  //Retorna raiz cubica
+
+console.log(Math.ceil(Math.random() * 10));
