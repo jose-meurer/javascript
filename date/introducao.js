0977f15428aad86d5367360cafe8a532fa8df094
joseMeurer <josewmeurer@gmail.com>
2023-11-28 11:13:52 -0300

    metodos strings date

--- date/introducao.js
+++ date/introducao.js
@@ -1,6 +1,6 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
-const date = new Date();
+let date = new Date();
 console.log(date);
 console.log(date.getDay());
 console.log(date.getMonth());
@@ -9,10 +9,24 @@ console.log(date.getFullYear());
 console.log(date.getHours());
 console.log(date.getUTCHours());
 console.log(Date.UTC(2023, 10, 28));
-console.log(Date.now())
+console.log(Date.now());
 console.log(date.getTimezoneOffset()); //Difenca da minha hora local para UTC(GMT-3)
 
 date.setDate(10);
 date.setFullYear(2012);
 date.setTime(0);
-console.log(date)
+console.log(date);
+
+console.log("\n\n-------\n");
+date = new Date();
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+console.log(date.toUTCString());
+console.log(date.toISOString());
+
+console.log(date.valueOf());
+console.log(date.toLocaleString("en-US"));
+console.log(date.toLocaleString());
+console.log(date.toLocaleString("pt-PT", {month: "long", weekday: "long", day: "numeric", year: "numeric"}));
